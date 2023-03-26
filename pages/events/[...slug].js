f702c67ccf5fb1a929d7c93c89c392dd8076f2bc
Oana Sasaran <sasaran.oana@gmail.com>
2023-03-26 21:19:31 +0300

    First commit to a main Branch

---
+++ pages/events/[...slug].js
@@ -0,0 +1,7 @@
+export default function FilterdEventsPage() {
+  return (
+    <div>
+      <h1>Filterd Events</h1>
+    </div>
+  );
+}
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,7 @@
 export default function AllEventsPage() {
   return (
     <div>
-      <h1>Events Page</h1>
+      <h1>All Events Page</h1>
     </div>
   );
 }
